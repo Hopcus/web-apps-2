963a1c8605d1ae0ed412a70f9c22c1d49e7be35b
Hopcus <alyssa.hopcus@kellogg.northwestern.edu>
2023-02-14 22:31:09 +0000

    praticed MVC with companies and tacostagram posts

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,15 @@
 class DiceController < ApplicationController
-
+    # every controller has several methods. this method is called an "action"
+    # here, the implicit method is:
+    # def index
+        # reder the file in /views/dice/index.html
+        # its looking for a file that matches dice?
+    # end
+    def index
+        @die1 = rand(1..6)
+        #adding the @ symbol makes it an instance so it can translate from backend to frontend, need to copy in the frontend
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+        @username = params["username"]
+    end
 end
